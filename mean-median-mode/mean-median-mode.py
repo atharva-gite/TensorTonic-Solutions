66a4e8734a08351d25c71469594bd665bf354263
Atharva Gite <124499569+atharva-gite@users.noreply.github.com>
2026-07-20 13:47:23 +0530

    Solve mean-median-mode

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -1,24 +1,8 @@
 import numpy as np
 from collections import Counter
 
-def calc_mean(x):
-    sum=0
-    for num in x:
-        sum+=num
-    return sum/len(x)
-def calc_median(x):
-    x=sorted(x)
-    length=len(x)
-    if length%2==1:
-        return x[length//2]
-    else:
-        return (x[length//2]+x[length//2-1])/2
-def calc_mode(x):
-    freq={}
-    for num in x:
-        freq[num]=freq.get(num,0)+1
-    max_freq=max(freq.values())
-    mode=[key for key, value in freq.items() if value == max_freq]
-    return min(mode)
 def mean_median_mode(x):
-    return calc_mean(x),calc_median(x),calc_mode(x)
+    counts=Counter(x)
+    max_freq=max(counts.values())
+    mode=min(num for num, freq in counts.items() if freq == max_freq)
+    return np.mean(x),np.median(x),mode
